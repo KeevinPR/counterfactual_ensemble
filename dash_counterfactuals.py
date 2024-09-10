b6e4f94bedf08333a20b2cf2b0d44e542388e39e
KeevinPR <kevinpr27@hotmail.com>
2024-09-10 12:02:58 +0000

    Hidding divs until the user selects a row

--- dash_counterfactuals.py
+++ dash_counterfactuals.py
@@ -22,21 +22,31 @@ app.layout = html.Div([
 
     html.Br(),
 
-    # Tabla para mostrar las variables predictoras (siempre presente en el layout)
-    html.H3("Predictor Variables"),
-    dash_table.DataTable(
-        id='predictor-table',
-        columns=[],  # Inicialmente vacío
-        data=[],  # Inicialmente vacío
-        page_size=10,
-        row_selectable='single',  # Selección de una fila
-        selected_rows=[]  # Filas seleccionadas
-    ),
+    # Contenedor para la tabla de variables predictoras y su título
+    html.Div([
+        html.H3("Predictor Variables"),
+        dash_table.DataTable(
+            id='predictor-table',
+            columns=[],  # Inicialmente vacío
+            data=[],  # Inicialmente vacío
+            page_size=10,
+            row_selectable='single',  # Selección de una fila
+            selected_rows=[]  # Filas seleccionadas
+        ),
+    ], id='predictor-container', style={'display': 'none'}),  # Ocultar el contenedor hasta que se cargue el archivo
 
     html.Br(),
 
     # Input - Mostrar fila seleccionada
-    html.Div(id='input-selected-row'),
+    html.Div([
+        html.H3("Selected Row"),
+        dash_table.DataTable(
+            id='selected-row-table',  # Tabla para mostrar la fila seleccionada
+            columns=[],  # Inicialmente vacío
+            data=[],  # Inicialmente vacío
+            page_size=1,  # Mostrar solo la fila seleccionada
+        )
+    ], style={'display': 'none'}, id='selected-row-container'),  # Ocultamos la tabla hasta que se seleccione una fila
 
     # Contenedor del Dropdown para seleccionar la clase
     html.Div([
@@ -44,33 +54,34 @@ app.layout = html.Div([
         dcc.Dropdown(id='class-selector')
     ], id='class-container', style={'display': 'none'}),  # Oculto hasta que se selecciona una fila
 
-    # Selector de modelos
-    html.H3("Number of Models"),
-    dcc.Dropdown(
-        id='model-selector',
-        options=[{'label': str(i), 'value': i} for i in range(1, 6)],
-        value=5  # Valor por defecto
-    ),
-
-    html.Br(),
-
-    # Botón de Run, ahora con la clase para centrarlo y aplicar estilo
+    # Contenedor para el selector de modelos y el botón Run
     html.Div([
-        html.Button('Run', id='run-button')
-    ], className="run-container"),  # Centrar el botón con la clase 'run-container'
+        html.H3("Number of Models"),
+        dcc.Dropdown(
+            id='model-selector',
+            options=[{'label': str(i), 'value': i} for i in range(1, 6)],
+            value=5  # Valor por defecto
+        ),
+        html.Br(),
+
+        # Botón de Run, ahora con la clase para centrarlo y aplicar estilo
+        html.Div([
+            html.Button('Run', id='run-button')
+        ], className="run-container"),  # Centrar el botón con la clase 'run-container'
+    ], id='model-container', style={'display': 'none'}),  # Oculto hasta que se seleccione una fila
 
     html.Br(),
 
-    # Resultados
-    html.H3("Results"),
-    dash_table.DataTable(
-        id='results-table',
-        columns=[],  # Inicialmente vacío
-        data=[]  # Inicialmente vacío
-    )
+    # Contenedor para los resultados
+    html.Div([
+        html.H3("Results"),
+        dash_table.DataTable(
+            id='results-table',
+            columns=[],  # Inicialmente vacío
+            data=[]  # Inicialmente vacío
+        )
+    ], id='results-container', style={'display': 'none'})  # Oculto hasta que se generen los resultados
 ])
-
-
 
 
 # Función para parsear el archivo CSV subido
@@ -87,10 +98,11 @@ def parse_contents(contents, filename):
         print(e)
         return None
 
-# Callback para actualizar la tabla de variables predictoras
+# Callback para actualizar la tabla de variables predictoras y mostrar el contenedor
 @app.callback(
     [Output('predictor-table', 'data'),
-     Output('predictor-table', 'columns')],
+     Output('predictor-table', 'columns'),
+     Output('predictor-container', 'style')],
     Input('upload-data', 'contents'),
     State('upload-data', 'filename')
 )
@@ -99,24 +111,25 @@ def update_predictor_table(contents, filename):
         df = parse_contents(contents, filename)
         columns = [{'name': i, 'id': i} for i in df.columns]
         data = df.to_dict('records')
-        return data, columns
-    return [], []
+        return data, columns, {'display': 'block'}
+    return [], [], {'display': 'none'}
 
-# Callback para mostrar la fila seleccionada en el input
+# Callback para mostrar la fila seleccionada en una tabla con el mismo formato
 @app.callback(
-    Output('input-selected-row', 'children'),
+    [Output('selected-row-table', 'data'),
+     Output('selected-row-table', 'columns'),
+     Output('selected-row-container', 'style'),
+     Output('model-container', 'style')],
     Input('predictor-table', 'selected_rows'),
     State('predictor-table', 'data')
 )
 def display_selected_row(selected_rows, data):
     if selected_rows is None or len(selected_rows) == 0:
-        return "No row selected."
+        return [], [], {'display': 'none'}, {'display': 'none'}
     selected_row = selected_rows[0]
-    row_data = data[selected_row]
-    return html.Div([
-        html.H4(f"Selected row: {selected_row}"),
-        html.P(f"Data: {row_data}")
-    ])
+    row_data = [data[selected_row]]  # Convertimos la fila seleccionada en una lista de un solo elemento
+    columns = [{'name': i, 'id': i} for i in data[selected_row].keys()]
+    return row_data, columns, {'display': 'block'}, {'display': 'block'}  # Mostramos la tabla y el contenedor de modelos
 
 # Callback para mostrar el dropdown de clases cuando se selecciona una fila
 @app.callback(
@@ -135,7 +148,7 @@ def update_class_selector(selected_rows, data):
     row_data = data[selected_row]
 
     # Mostrar el contenido del row_data para verificar la estructura
-    print(f"Datos seleccionados: {row_data}")
+    print(f"Selected Data: {row_data}")
 
     # Asegurarnos de que existe la columna 'class'
     if 'class' not in row_data:
@@ -152,7 +165,8 @@ def update_class_selector(selected_rows, data):
 # Callback para ejecutar la lógica contrafactual y mostrar los resultados
 @app.callback(
     [Output('results-table', 'data'),
-     Output('results-table', 'columns')],
+     Output('results-table', 'columns'),
+     Output('results-container', 'style')],
     Input('run-button', 'n_clicks'),
     State('predictor-table', 'selected_rows'),
     State('class-selector', 'value'),
@@ -161,7 +175,7 @@ def update_class_selector(selected_rows, data):
 )
 def run_counterfactual(n_clicks, selected_rows, new_class, num_models, data):
     if n_clicks is None or len(selected_rows) == 0:
-        return [], []
+        return [], [], {'display': 'none'}
 
     # Obtener la fila seleccionada
     selected_row = selected_rows[0]
@@ -179,7 +193,7 @@ def run_counterfactual(n_clicks, selected_rows, new_class, num_models, data):
     results_data = [original_row, new_row]
     columns = [{'name': i, 'id': i} for i in row_data.keys()]
     
-    return results_data, columns
+    return results_data, columns, {'display': 'block'}
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
