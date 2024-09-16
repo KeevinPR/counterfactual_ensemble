6c88a865fa4cb3a2eb7e82449d6371b58a99b00e
KeevinPR <kevinpr27@hotmail.com>
2024-09-16 11:46:16 +0000

    updating R path to ubuntu server

--- ensemble_counterfactuals/__init__.py
+++ ensemble_counterfactuals/__init__.py
@@ -1,6 +1,10 @@
 import warnings
 import os
+from .common_funcs import train_models, model_ensemble, inicialize_ensemble, ensemble_selector
+from .codification import Codificacion
+from . import algorithms
 
 # Configurar la variable de entorno R_HOME
-os.environ['R_HOME'] = 'C:\Program Files\R\R-4.3.2'
+#os.environ['R_HOME'] = 'C:\Program Files\R\R-4.3.2'
+os.environ['R_HOME'] = '/usr/lib/R'  # For Ubuntu
 warnings.filterwarnings('ignore')
