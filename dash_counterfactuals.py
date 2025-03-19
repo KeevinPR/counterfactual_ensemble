0738a6064340600ab05c0e0ed4a721a3b5204314
KeevinPR <kevinpr27@hotmail.com>
2025-03-19 11:42:19 +0000

    minor change in debuging syntax

--- dash_counterfactuals.py
+++ dash_counterfactuals.py
@@ -477,4 +477,4 @@ def parse_contents(contents, filename):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
